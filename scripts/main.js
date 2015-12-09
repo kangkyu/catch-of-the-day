bd00098bc5c6b716534431c1385a0f43d3045028
kangkyu <kangkyu1111@gmail.com>
2015-12-09 13:12:46 -0800

    commit 8 - event refs History mixins

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
@@ -69,11 +70,17 @@ var Inventory = React.createClass({
 */
 
 var StorePicker = React.createClass({
+  mixins : [History],
 
+  goToStore : function(event) {
+    event.preventDefault();
+    console.log(this.refs.storeId);
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId);
+  },
   render : function() {
     return (
-      <form className="store-selector">
-        {/* creating a store  */}
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
         <input type="text" ref="storeId" defaultValue={h.getFunName()} />
         <input type="Submit" />
